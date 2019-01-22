36d047d505a202c5600946c04d03a2b96f10ef95
Fedor Korotkov <fedor.korotkov@gmail.com>
2019-01-22 09:36:34 -0500

    Rebase action

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# rebase
-GitHub Action to automatically rebase PRs
+# GitHub action to automatically rebase PRs
+
+[![Build Status](https://api.cirrus-ci.com/github/cirrus-actions/rebase.svg)](https://cirrus-ci.com/github/cirrus-actions/rebase) [![](https://images.microbadger.com/badges/version/cirrusactions/rebase.svg)](https://microbadger.com/images/cirrusactions/rebase) [![](https://images.microbadger.com/badges/image/cirrusactions/rebase.svg)](https://microbadger.com/images/cirrusactions/rebase)
+
+# Limitations
+
+Due to current [limitations of GitHub Actions](https://developer.github.com/actions/):
+
+> GitHub Actions is limited to private repositories and push events in public repositories during the limited public beta.
+
+*Rebase* action currently only works on private repositories since it requires `IssueCommentEvent` event.
+
+# Installation
+
+To configure the action simply add the following lines to your `.github/main.workflow` workflow file:
+
+```
+workflow "Automatic Rebase" {
+  on = "issue_comment"
+  resolves = "Rebase"
+}
+
+action "Rebase" {
+  uses = "docker://cirrusactions/rebase:latest"
+  secrets = ["GITHUB_TOKEN"]
+}
+```
---
+++ entrypoint.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -e
+
+# skip if no /rebase
+echo "Checking if contains '/rebase' command..."
+(jq -r ".comment.body" "$GITHUB_EVENT_PATH" | grep -E "/rebase") || exit 78
+
+# skip if not a PR
+echo "Checking if a PR command..."
+(jq -r ".issue.pull_request.url" "$GITHUB_EVENT_PATH") || exit 78
+
+if [[ "$(jq -r ".action" "$GITHUB_EVENT_PATH")" != "created" ]]; then
+	echo "This is not a new comment event!"
+	exit 78
+fi
+
+PR_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
+REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+echo "Collecting information about PR #$PR_NUMBER of $REPO_FULLNAME..."
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
+
+URI=https://api.github.com
+API_HEADER="Accept: application/vnd.github.v3+json"
+AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+
+pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
+          "${URI}/repos/$REPO_FULLNAME/pulls/$PR_NUMBER")
+
+BASE_REPO=$(echo "$pr_resp" | jq -r .base.repo.full_name)
+BASE_BRANCH=$(echo "$pr_resp" | jq -r .base.ref)
+
+if [[ "$(echo "$pr_resp" | jq -r .rebaseable)" != "true" ]]; then
+	echo "GitHub doesn't think that the PR is rebaseable!"
+	exit 1
+fi
+
+if [[ -z "$BASE_BRANCH" ]]; then
+	echo "Cannot get base branch information for PR #$PR_NUMBER!"
+  echo "API response: $pr_resp"
+	exit 1
+fi
+
+HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
+HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
+
+echo "Base branch for PR #$PR_NUMBER is $BASE_BRANCH"
+
+if [[ "$BASE_REPO" != "$HEAD_REPO" ]]; then
+	echo "PRs from forks are not supported at the moment."
+	exit 1
+fi
+
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+git config --global user.email "action@github.com"
+git config --global user.name "GitHub Action"
+
+set -o xtrace
+
+# make sure branches are up-to-date
+git fetch origin $BASE_BRANCH
+git fetch origin $HEAD_BRANCH
+
+# do the rebase
+git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
+git rebase origin/$BASE_BRANCH
+
+# push back
+git push --force
