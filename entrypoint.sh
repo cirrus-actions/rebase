54b37dca13e18fbe5f22f408ac37a49548b8b81b
Luke Bjerring <luke@faire.com>
2020-06-19 07:30:58 -0400

    Add support for pull_request webhook events (#55)
    * Use pull_request not issue
    
    * Fall back to issue number, exit if still null
    
    * Fix variable use

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-PR_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
+PR_NUMBER=$(jq -r ".pull_request.number" "$GITHUB_EVENT_PATH")
+if [[ "$PR_NUMBER" == "null" ]]; then
+	PR_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
+fi
+if [[ "$PR_NUMBER" == "null" ]]; then
+	echo "Failed to determine PR Number."
+	exit 1
+fi
 echo "Collecting information about PR #$PR_NUMBER of $GITHUB_REPOSITORY..."
 
 if [[ -z "$GITHUB_TOKEN" ]]; then
