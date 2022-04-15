99b0226479c59e6a40092b8afe704aabc61dec09
fischman-bcny <ami-github-emails-aaaaf75vldllkcgdrw2p2ypzqe@thebrowsercompany.slack.com>
2022-04-15 13:23:09 -0700

    Unbreak rebase following cirrus-actions/rebase#92. (#94)

--- entrypoint.sh
+++ entrypoint.sh
@@ -83,6 +83,9 @@ USER_TOKEN=${USER_LOGIN//-/_}_TOKEN
 UNTRIMMED_COMMITTER_TOKEN=${!USER_TOKEN:-$GITHUB_TOKEN}
 COMMITTER_TOKEN="$(echo -e "${UNTRIMMED_COMMITTER_TOKEN}" | tr -d '[:space:]')"
 
+# See https://github.com/actions/checkout/issues/766 for motivation.
+git config --global --add safe.directory /github/workspace
+
 git remote set-url origin https://x-access-token:$COMMITTER_TOKEN@github.com/$GITHUB_REPOSITORY.git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
